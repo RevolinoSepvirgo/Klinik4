4c83db0bd63d46c5a2d5e5a22d0081d603d52be1
RevolinoSepvirgo <revolinosepvirgo02@gmail.com>
2024-10-16 21:20:46 +0700

    update

---
+++ app/src/main/java/com/example/klinikmi2a/PageLogin.kt
@@ -0,0 +1,30 @@
+package com.example.klinikmi2a
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PageLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_page_login)
+        val textSignUp: TextView = findViewById(R.id.textSignUp)
+
+        textSignUp.setOnClickListener {
+            val intent = Intent(this, PageRegister::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        }
+    }
